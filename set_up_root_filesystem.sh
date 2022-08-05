1b9f33a97caef33688637d01d1d980ad4de2a18c
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2022-08-05 11:46:41 +0100

    Add a shell script to set up a root filesystem for the container

---
+++ set_up_root_filesystem.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+alpine_filename="alpine-minirootfs-3.16.1-x86_64.tar.gz"
+
+download_alpine () {
+    if [ ! -e $alpine_filename ]
+    then
+        echo "Downloading Alpine"
+        curl \
+            --output $alpine_filename \
+            "https://dl-cdn.alpinelinux.org/alpine/v3.16/releases/x86_64/$alpine_filename"
+    else
+        echo "Found Alpine, not downloading"
+    fi
+
+    echo
+}
+
+create_container_root_filesystem () {
+    download_alpine
+
+    directory=$1
+
+    echo "Creating container root filesystem in $directory"
+
+    rm -rf $directory && mkdir $directory
+    tar -C $directory -xf $alpine_filename
+
+    echo
+}
+
+create_container_root_filesystem "container_root"
